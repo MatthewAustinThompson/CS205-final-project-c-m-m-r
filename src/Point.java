04cbb6d0a33e9b0e344f31d4427b7dc09c705675
marcus-elia <mselia@uvm.edu>
2020-03-24 22:31:22 -0400

    Added the Point class

---
+++ src/Point.java
@@ -0,0 +1,11 @@
+// The Point class is essentially a struct. It contains an
+// x and y coordinate, which are both public
+public class Point
+{
+    public double x, y;
+    public Point(double inputX, double inputY)
+    {
+        x = inputX;
+        y = inputY;
+    }
+}
