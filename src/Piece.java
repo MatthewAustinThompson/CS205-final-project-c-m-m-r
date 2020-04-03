045f0f2d1f46baa530c5d6e2da0875c38693dab6
marcus-elia <mselia@uvm.edu>
2020-04-03 13:23:39 -0400

    Added more template stuff for moving Pieces

--- src/Piece.java
+++ src/Piece.java
@@ -1,23 +1,55 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public abstract class Piece
 {
+    // ========================================
+    //
+    //            Basic properties
+    //
+    // ========================================
     protected GameManager manager;
+    protected Board board;
+    protected Team team;
+    protected PieceType pieceType;
 
-    // The current center of the Piece
-    protected Point center;
+    // ========================================
+    //
+    //         Board and Movement Things
+    //
+    // ========================================
 
     // The location of the Piece in terms of the board
     protected BoardPoint location;
 
+    // The squares on the board this Piece is looking at
+    // (open squares and squares with other Pieces)
+    protected ArrayList<BoardPoint> targetingSquares;
+
+    // The squares on the Board this Piece can actually move to
+    protected ArrayList<BoardPoint> legalMoveSquares;
+
+    // ========================================
+    //
+    //           Drawing the Piece
+    //
+    // ========================================
+
+    // The current center of the Piece
+    protected Point center;
+    // Colors for drawing
     protected Color fillColor;
     protected Color symbolColor;
 
+    // Constructor
     public Piece(GameManager inputManager, Point inputCenter, BoardPoint inputLocation)
     {
         manager = inputManager;
+        board = manager.getBoard();
         center = inputCenter;
         location = inputLocation;
+        targetingSquares = new ArrayList<BoardPoint>();
+        legalMoveSquares = new ArrayList<BoardPoint>();
     }
 
     public abstract void tick();
@@ -33,6 +65,23 @@ public abstract class Piece
     {
         return location;
     }
+    public Team getTeam()
+    {
+        return team;
+    }
+    public PieceType getPieceType()
+    {
+        return pieceType;
+    }
+    public ArrayList<BoardPoint> getTargetingSquares()
+    {
+        return targetingSquares;
+    }
+    public ArrayList<BoardPoint> getLegalMoveSquares()
+    {
+        return legalMoveSquares;
+    }
+
 
     // Setters
     public void setCenter(Point inputCenter)
@@ -42,5 +91,33 @@ public abstract class Piece
     public void setLocation(BoardPoint inputLocation)
     {
         location = inputLocation;
+    }
+    public void setTeam(Team t)
+    {
+        team = t;
+    }
+    public void setPieceType(PieceType pt)
+    {
+        pieceType = pt;
+    }
+
+    // Moving
+    // This sets targetingSquares to contain all squares that this Piece is
+    // able to move to, or capture at, or protect teammates at
+    public abstract void findTargetingSquares();
+
+    // This sets legalMoveSquares to contain every square in targetingSquares
+    // that doesn't contain a teammate
+    public void findLegalMoveSquares()
+    {
+        legalMoveSquares = new ArrayList<BoardPoint>();
+        for(BoardPoint bp : targetingSquares)
+        {
+            // If there's not a teammate there, we can move there
+            if(!board.containsTeammate(this, bp))
+            {
+                legalMoveSquares.add(bp);
+            }
+        }
     }
 }
