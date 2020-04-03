4c644ae4a99dd71fd836a2fe7aba971c9b40d178
marcus-elia <mselia@uvm.edu>
2020-04-03 14:49:33 -0400

    Moved code into Piece class

--- src/Horse.java
+++ src/Horse.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 public class Horse extends Piece
 {
 
-    // The octagon shape of the piece
-    private Path2D.Double outline;
 
     public Horse(GameManager inputManager, Point inputCenter, BoardPoint inputLocation, Team inputTeam)
     {
@@ -52,32 +50,7 @@ public class Horse extends Piece
         g2d.drawString("Horse", (int)this.center.x - pixelLength/2, (int)center.y + 5);
     }
 
-    public void initializeOctagon()
-    {
-        // Octagon geometry stuff
-        int sideLength = 20;
-        double r1 = sideLength/2.0;
-        double r2 = sideLength*(1 + Math.sqrt(2))/2;
 
-        // Trace out an octagon
-        // A Path2D is just a sequence of points that
-        // specifies line segments
-        outline = new Path2D.Double();
-
-        // Start at the top left
-        outline.moveTo(center.x - r1, center.y - r2);
-
-        // Go around clockwise
-        outline.lineTo(center.x + r1, center.y - r2);
-        outline.lineTo(center.x + r2, center.y - r1);
-        outline.lineTo(center.x + r2, center.y + r1);
-        outline.lineTo(center.x + r1, center.y + r2);
-        outline.lineTo(center.x - r1, center.y + r2);
-        outline.lineTo(center.x - r2, center.y + r1);
-        outline.lineTo(center.x - r2, center.y - r1);
-        // End where you started
-        outline.lineTo(center.x - r1, center.y - r2);
-    }
 
     @Override
     public void findTargetingSquares()
