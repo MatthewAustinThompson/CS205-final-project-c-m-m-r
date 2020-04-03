c2724e6bb1b71d8507ce80a0a0c7b176cbe9017e
marcus-elia <mselia@uvm.edu>
2020-04-03 14:48:45 -0400

    Clicking on a Piece highlights squares it can move to

--- src/GameManager.java
+++ src/GameManager.java
@@ -11,13 +11,18 @@ public class GameManager
 
     private Board board;
 
+    // When the player clicks on a Piece
+    private Piece selectedPiece;
+
     public GameManager(int inputWidth, int inputHeight)
     {
         width = inputWidth;
         height = inputHeight;
 
         pieces = new ArrayList<Piece>();
-        pieces.add(new ExamplePieceMarcus(this,
+
+        selectedPiece = null;
+        /*pieces.add(new ExamplePieceMarcus(this,
                 new Point(width/2, height/2), new BoardPoint(0,0)));
 
         pieces.add(new ExamplePieceRuth(this,
@@ -27,9 +32,12 @@ public class GameManager
                 new Point(width/2, height/4) , new BoardPoint(2,1)));
 
         pieces.add(new ExamplePieceMatthew(this,
-                new Point(width/4, height/6) , new BoardPoint(2,2)));
+                new Point(width/4, height/6) , new BoardPoint(2,2)));*/
 
         board = new Board(this);
+
+        this.addPiece(PieceType.Horse, Team.Player, new BoardPoint(4,5));
+        this.updatePieces();
     }
 
     public void tick()
@@ -74,5 +82,68 @@ public class GameManager
     public void setHeight(int inputHeight)
     {
         height = inputHeight;
+    }
+
+    // ================================
+    //
+    //         Managing Pieces
+    //
+    // ================================
+    public void addPiece(PieceType pieceType, Team team, BoardPoint location)
+    {
+        if(board.containsPiece(location))
+        {
+            System.out.println("Error: cannot insert a Piece in non-empty space.");
+            return;
+        }
+        Piece p;
+        Point center = board.boardPointToPoint(location);
+        switch(pieceType)
+        {
+            case Horse:
+                p = new Horse(this, center, location, team);
+                break;
+            default:
+                return;
+        }
+        pieces.add(p);
+        board.getSpaces()[location.getX()][location.getY()] = p;
+    }
+    public void updatePieces()
+    {
+        for(Piece p : pieces)
+        {
+            p.update();
+        }
+    }
+
+
+    // ================================
+    //
+    //           User Input
+    //
+    // ================================
+    public void reactToClick(int mx, int my)
+    {
+        if(selectedPiece != null)
+        {
+            selectedPiece = null;
+            board.unhighlightAll();
+            return;
+        }
+        else
+        {
+            for(Piece p : pieces)
+            {
+                if(p.containsClick(mx, my))
+                {
+                    selectedPiece = p;
+                    board.highlightLegalMoves(p);
+                    return;
+                }
+            }
+        }
+        selectedPiece = null;
+        board.unhighlightAll();
     }
 }
