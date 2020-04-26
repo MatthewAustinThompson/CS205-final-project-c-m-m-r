6f3f12cc9d7e633207245561f8a7b3b1f08f9be9
ruthkollar <kollar.ruth@gmail.com>
2020-04-26 16:36:55 -0400

    Added Start Screen icons decorations

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -1,6 +1,10 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.Path2D;
 import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class StartScreen
@@ -61,12 +65,17 @@ public class StartScreen
     // ===============================
     public void render(Graphics2D g2d)
     {
-        // ==============NAMES=================
-
 
         // ==============BACKGROUND=================
-        g2d.setColor(bgColor);
+        g2d.setColor(new Color(75,125,191));
         g2d.fillRect(0, 0, windowWidth, windowHeight);
+
+        g2d.setColor(paleBlue);
+        int centerWidth = (int)(.98 * windowWidth);
+        int centerHeight = (int)(.96 * windowHeight);
+        int centerX = windowWidth/2 - centerWidth/2;
+        int centerY = windowHeight/2 - centerHeight/2;
+        g2d.fillRect(centerX, centerY, centerWidth, centerHeight);
 
         // ==============FONT=================
         Font font = new Font("Helvetica", Font.PLAIN, 54);
@@ -74,6 +83,71 @@ public class StartScreen
 
         //Font smallerFont = new Font("Helvetica", Font.PLAIN, 40);
         //FontMetrics smallerMetrics = g2d.getFontMetrics(smallerFont);
+
+        // ==================ICONS DECORATION====================
+
+        BufferedImage img = null;
+
+        //Graphics2d object.drawImage(Image object, x coord, y coord, idk but set null)
+        try {
+            /*
+            img = ImageIO.read(new File("./src/Images/ChariotRookRed.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .9), null);
+            img = ImageIO.read(new File("./src/Images/ChariotRookGreen.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .9), null);
+
+             */
+
+            img = ImageIO.read(new File("./src/Images/HorseRed.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .8), null);
+            img = ImageIO.read(new File("./src/Images/HorseGreen.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .8), null);
+
+            img = ImageIO.read(new File("./src/Images/ElephantRed2.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .7), null);
+            img = ImageIO.read(new File("./src/Images/ElephantGreen2.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .7), null);
+
+            img = ImageIO.read(new File("./src/Images/GuardRed.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .6), null);
+            img = ImageIO.read(new File("./src/Images/GuardGreen.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .6), null);
+
+            //
+
+            img = ImageIO.read(new File("./src/Images/KingRed.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .5), null);
+            img = ImageIO.read(new File("./src/Images/KingGreen.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .5), null);
+
+            //
+
+            img = ImageIO.read(new File("./src/Images/GuardRed.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .4), null);
+            img = ImageIO.read(new File("./src/Images/GuardGreen.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .4), null);
+
+            img = ImageIO.read(new File("./src/Images/ElephantRed2.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .3), null);
+            img = ImageIO.read(new File("./src/Images/ElephantGreen2.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .3), null);
+
+            img = ImageIO.read(new File("./src/Images/HorseRed.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .2), null);
+            img = ImageIO.read(new File("./src/Images/HorseGreen.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .2), null);
+
+            /*
+            img = ImageIO.read(new File("./src/Images/ChariotRookRed.png"));
+            g2d.drawImage(img,windowWidth/8, (int)(windowHeight * .1), null);
+            img = ImageIO.read(new File("./src/Images/ChariotRookGreen.png"));
+            g2d.drawImage(img,7 * windowWidth/8 - 40, (int)(windowHeight * .1), null);
+
+             */
+
+        } catch (IOException e) {
+            System.out.println("no img found");
+        }
 
 
 
@@ -117,18 +191,23 @@ public class StartScreen
         g2d.drawString(buttonText,stringPosX,stringPosY);*/
 
         // ==============DRAW CAPTION TEXT=================
+        Font captionFont = new Font("Helvetica", Font.PLAIN, 14);
+        FontMetrics metrics2 = g2d.getFontMetrics(captionFont);
+
         String captionTxt1 = "Capture the computer's general to win!";
         String captionTxt2 = "Click on a piece to see available moves.";
         String captionTxt3 = "(It's ok if you don't already know the rules, our system will help you.)";
-        int captionX = (int) (windowWidth / 2 - metrics.getStringBounds(captionTxt1,null).getWidth()/6);
+        int captionX = (int) (windowWidth / 2 - metrics2.getStringBounds(captionTxt1,null).getWidth()/2);
+        int captionX2 = (int) (windowWidth / 2 - metrics2.getStringBounds(captionTxt2,null).getWidth()/2);
+        int captionX3 = (int) (windowWidth / 2 - metrics2.getStringBounds(captionTxt3,null).getWidth()/2);
         int captionY = windowHeight * 8 / 10;
 
-        Font captionFont = new Font("Helvetica", Font.BOLD, 18);
+
         g2d.setFont(captionFont);
         g2d.setColor(txtColor);
         g2d.drawString(captionTxt1, captionX, captionY);
         g2d.drawString(captionTxt2, captionX, captionY + 25);
-        g2d.drawString(captionTxt3, captionX, captionY + 50);
+        g2d.drawString(captionTxt3, captionX3, captionY + 50);
     }
 
     public boolean buttonPress(int mx, int my)
